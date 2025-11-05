60b5daf18b419a78fa581977438b54b25b4e490a
Rafi Putra Nugraha <18224079@std.stei.itb.ac.id>
2025-11-05 09:23:29 +0700

    update: added praprak8 initial files

--- oop/prak/MeasurablePair/PairComparator.java
+++ oop/prak/MeasurablePair/PairComparator.java
@@ -6,11 +6,7 @@ public final class PairComparator {
     // 1. Lengkapi parameter generik method yang merupakan kelas-kelas yang
     // menerapkan interface Comparable
     // 2. Lengkapi parameter generik dari pair yang dibandingkan
-    public static <T extends Measurable<T>, R extends Measurable<R>> boolean isGreater(Pair<? extends T, ? extends R> p1, Pair<? extends T, ? extends R> p2) {
-        // TODO:
-        // 1. Bandingkan elemen pertama dan kedua antar pair menggunakan compareTo().
-        // 2. Kembalikan true hanya jika kedua perbandingan menunjukkan p1 lebih besar dari p2.
-        // ...
-        return p1.getFirst().compareTo(p2.getFirst()) > 0 && p1.getSecond().compareTo(p2.getSecond()) > 0;
-    }
+    public static <T extends Measurable<T>, R extends Measurable<R>, U extends T, V extends R> boolean isGreater(Pair<T, R> p1, Pair<U, V> p2) {
+    return p1.getFirst().compareTo(p2.getFirst()) > 0 && p1.getSecond().compareTo(p2.getSecond()) > 0;
+}
 }
